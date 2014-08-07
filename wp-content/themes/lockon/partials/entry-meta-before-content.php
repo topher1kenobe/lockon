374cb9e01eadebfbaf353e8f9a8f3adc2623a087
Topher <topher@x-team.com>
2014-08-07 19:11:53 -0400

    Make the meta file work properly

--- wp-content/themes/lockon/partials/entry-meta-before-content.php
+++ wp-content/themes/lockon/partials/entry-meta-before-content.php
@@ -1,4 +1,15 @@
 <?php
-echo '<p class="document_types">Type: ';
-the_terms( get_the_ID(), 'documenttype' );
-echo '</p>';
+/**
+ * @package Make
+ */
+
+if ( is_singular( 'documents' ) || is_archive( 'documents' ) ) {
+	echo '<p class="document_types">Type: ';
+	the_terms( get_the_ID(), 'documenttype' );
+	echo '</p>';
+} else {
+	global $ttfmake_current_location;
+	$ttfmake_current_location = 'before-content';
+	get_template_part( 'partials/entry', 'meta' );
+	unset( $ttfmake_current_location );
+}
